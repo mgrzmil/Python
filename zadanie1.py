b9fee14ed4064eab325650f9899b3941c0bbff7e
mgrzmil <mgrzmil@users.noreply.github.com>
2020-12-05 14:18:08 +0100

    zdania python (2)

---
+++ zadanie1.py
@@ -0,0 +1,7 @@
+animals = ['cat','dog']
+
+print(animals[0])
+print(animals[1])
+
+for s in animals:
+    print(s)
---
+++ zadanie2.py
@@ -0,0 +1,9 @@
+animals = ['cat', 'dog','elephant']
+print(animals[0])
+print(animals[1])
+for a in animals:
+    print(a)
+for idx in range( len(animals) ) :
+    print("idx: " + str(idx) + " : " + animals[idx])
+
+print(animals[2])
