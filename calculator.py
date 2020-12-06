4cef0c129786b86fc163e9129f9196bff2a8b98e
mgrzmil <michal.grzmil@vp.pl>
2020-12-06 12:34:26 +0000

    git add

---
+++ calculator.py
@@ -0,0 +1,9 @@
+def calculate(a, b, operacja):
+  if operacja == "+":
+    return a + b
+  elif operacja == "-":
+    return a - b
+  elif operacja =="*":
+    return a * b
+  elif operacja =="/"
+    return a / b
